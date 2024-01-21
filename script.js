8db6adc854a3230b5803a1291bcd540812119090
unknown <kreede929@gmail.com>
2024-01-22 01:30:49 +0500

    new items

---
+++ script.js
@@ -0,0 +1,26 @@
+const data = 'Ulugbek';
+
+const array = [
+    {
+        id: '1',
+        name: 'Ulugbek'
+    },
+    {
+        id: '1',
+        name: 'Ulugbek'
+    },
+    {
+        id: '1',
+        name: 'Ulugbek'
+    },
+    {
+        id: '1',
+        name: 'Ulugbek'
+    },
+
+]
+
+console.log('Ulugbek', array)
+console.log('Bahriddin', array)
+console.log('Javohir', data)
+console.log('Jamshid', data)
